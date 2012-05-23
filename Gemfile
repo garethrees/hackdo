5ace961b92a8736a3adfd1cbb47a9f289e9c7fc3
Gareth Rees <gareth.rees@boxuk.com>
2012-05-23 14:47:56 +0100

    Initial Commit

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "http://rubygems.org"
 gem "sinatra"
 gem "pony"
 gem "xml-sitemap"
+gem "grit"
 
 group :development do
   gem 'sass'
--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra/base'
+require 'grit'
+require 'fileutils'
 
 class App < Sinatra::Base
   set :root, File.dirname(__FILE__)
@@ -7,6 +9,14 @@ class App < Sinatra::Base
 
   get "/" do
   	erb :index
+  end
+
+  get "/repo" do
+      repo = Grit::Git.new('/tmp/repo')
+      @process = repo.clone({:process_info => true, :progress => true, :timeout => false}, "#{params[:repo_url]}", "tmp/test")
+      @todos = %x{grep -RIn TODO #{File.dirname(__FILE__)}/tmp/test}
+      FileUtils.rm_rf("#{File.dirname(__FILE__)}/tmp/test")
+      erb :index
   end
 
   # HELPERS #
